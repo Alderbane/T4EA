df84d11b9bca7b671d4da16108ea73885c0f5ac7
Alderbane <alderban@outlook.com>
2019-10-24 07:38:21 -0500

    Update T4.java

--- T4.java
+++ T4.java
@@ -1,102 +1,132 @@
 import java.util.Arrays;
 
 public class T4 {
+	static int calls = 0;
+
 	public static int indexOf(String[] arr, String c) {
-		if (arr.length == 0)
+		calls = 0;
+		return indexOf(arr, 0, arr.length - 1, c);
+
+	}
+
+	static int indexOf(String[] array, int left, int right, String key) {
+		if (left > right)
 			return -1;
-		int half = arr.length / 2;
-		if (arr[half] == c)
-			return half;
-		if (arr[half] == "") {
-			String[] h1 = Arrays.copyOfRange(arr, 0, half);
-			String[] h2 = Arrays.copyOfRange(arr, half + 1, arr.length);
-			int iH1 = indexOf(h1, c);
-			int iH2 = indexOf(h2, c);
+		calls++;
+		int middle = (left + right) / 2; // D(N)
+		if (array[middle] == "") {
+			int iH1 = indexOf(array, left, middle - 1, key);
+			int iH2 = indexOf(array, middle + 1, right, key);
 			if (iH1 >= 0)
 				return iH1;
-			if (iH2 < 0)
-				return -1;
-			return half + iH2 + 1;
+			else
+				return iH2;
 		}
-		if (arr[half].compareTo(c) > 0) {
-			String[] h1 = Arrays.copyOfRange(arr, 0, half);
-			int iH1 = indexOf(h1, c);
-			if (iH1 >= 0)
-				return iH1;
-			return -1;
-		}
-		if (arr[half].compareTo(c) < 0) {
-			String[] h2 = Arrays.copyOfRange(arr, half + 1, arr.length);
-			int iH2 = indexOf(h2, c);
-			if (iH2 >= 0)
-				return half + iH2 + 1;
-			return -1;
-		}
-		return 0;
+		if (array[middle] == key)
+			return middle; // T(N/2)
+		if (key.compareTo(array[middle]) > 0)
+			return indexOf(array, middle + 1, right, key);
+		else
+			return indexOf(array, left, middle - 1, key);
 	}
 
 	public static boolean exists;
 
 	public static int indexOf(int[] arr, int n) {
-		
-		if (arr.length == 0){
-			exists = false;
-			return 0;
-		}
-		if (arr.length == 1 && arr[0] != n) {
-			exists = false;	
-			if(arr[0] > n) return 1;
-			return 0;
-			// return (arr[0]<n)?1:-1;
-		}
-		int half = (arr.length / 2);
-		if (arr[half] == n){
-			exists = true;
-			return half;
-		}
+		calls = 0;
+		exists = true;
+		int i = indexOf(arr, 0, arr.length - 1, n);
+		if (exists) {
+			return i;
+		} else
+			return -1 * (i + 1);
 
-		if (arr[half] > n) {
-			int[] h1 = Arrays.copyOfRange(arr, 0, half);
-			int iH1 = indexOf(h1, n);
-			if (exists)
-				return iH1;
-			else
-				return -1 * (1 - iH1);
-		}
-		if (arr[half] < n) {
-			int[] h2 = Arrays.copyOfRange(arr, half , arr.length);
-			int iH2 = indexOf(h2, n);
+		// if (arr.length == 0){
+		// exists = false;
+		// return 0;
+		// }
+		// if (arr.length == 1 && arr[0] != n) {
+		// exists = false;
+		// if(arr[0] > n) return 1;
+		// return 0;
+		// // return (arr[0]<n)?1:-1;
+		// }
+		// int half = (arr.length / 2);
+		// if (arr[half] == n){
+		// exists = true;
+		// return half;
+		// }
 
-			if (exists)
-				return half + iH2;
-			else
-				return -1 * (half - iH2 + 1);
-		}
+		// if (arr[half] > n) {
+		// int[] h1 = Arrays.copyOfRange(arr, 0, half);
+		// int iH1 = indexOf(h1, n);
+		// if (exists)
+		// return iH1;
+		// else
+		// return -1 * (1 - iH1);
+		// }
+		// if (arr[half] < n) {
+		// int[] h2 = Arrays.copyOfRange(arr, half , arr.length);
+		// int iH2 = indexOf(h2, n);
 
-		return 0;
+		// if (exists)
+		// return half + iH2;
+		// else
+		// return -1 * (half - iH2 + 1);
+		// }
+
+		// return 0;
 	}
 
-	// public static int[] maxsubarray(int[] arr) {
-	// int left, half, right;
-	// left = 0;
-	// half = arr.length/2 - 1;
-	// right = arr.length-1;
-	// if()
+	static int indexOf(int[] array, int left, int right, int key) {
+		if (left > right) {
+			exists = false;
+			return left;
+		}
 
-	// }
+		calls++;
+		int middle = (left + right) / 2; // D(N)
+		if (array[middle] == key)
+			return middle; // T(N/2)
+		if (key > array[middle])
+			return indexOf(array, middle + 1, right, key);
+		else
+			return indexOf(array, left, middle - 1, key);
+	}
+
+	public static int[] maxsubarray(int[] arr) {
+		calls = 0;
+		int[] ded = new int[2];
+		ded[0] = indexOf(arr, 0, arr.length - 1);
+		ded[1] = arr.length - 1;
+		return ded;
+	}
+
+	static int indexOf(int[] array, int left, int right) {
+		if (left > right)
+			return left;
+		calls++;
+		int middle = (left + right) / 2; // D(N)
+		if (array[middle] == 0)
+			return middle; // T(N/2)
+		if (0 > array[middle])
+			return indexOf(array, middle + 1, right);
+		else
+			return indexOf(array, left, middle - 1);
+	}
 
 	public static void main(String[] args) {
-		// String[] array = { "a", "b", "c", "d", "e", "", "", "f", "h", "", "i", "j" };
-		// System.out.println(indexOf(array, "a")); //0
-		// System.out.println(indexOf(array, "b")); //1
-		// System.out.println(indexOf(array, "c")); //2
-		// System.out.println(indexOf(array, "d")); //3
-		// System.out.println(indexOf(array, "e")); //4
-		// System.out.println(indexOf(array, "f")); //7
-		// System.out.println(indexOf(array, "g")); //-1
-		// System.out.println(indexOf(array, "h")); //8
-		// System.out.println(indexOf(array, "i")); //10
-		// System.out.println(indexOf(array, "j")); //11
+		String[] array = { "a", "b", "c", "d", "e", "", "", "f", "h", "", "i", "j" };
+		System.out.println(indexOf(array, "a")); // 0
+		System.out.println(indexOf(array, "b")); // 1
+		System.out.println(indexOf(array, "c")); // 2
+		System.out.println(indexOf(array, "d")); // 3
+		System.out.println(indexOf(array, "e")); // 4
+		System.out.println(indexOf(array, "f")); // 7
+		System.out.println(indexOf(array, "g")); // -1
+		System.out.println(indexOf(array, "h")); // 8
+		System.out.println(indexOf(array, "i")); // 10
+		System.out.println(indexOf(array, "j")); // 11
 
 		System.out.println("---------------------------");
 
@@ -110,5 +140,14 @@ public class T4 {
 		System.out.println(indexOf(arr, 2)); // -4
 		System.out.println(indexOf(arr, 3)); // 3
 		System.out.println(indexOf(arr, 4)); // -5
+
+		System.out.println("----------------------");
+
+		int[] ded = { -5, -4, -3, -1, 0, 1, 3 };
+		for (int i : maxsubarray(ded)) {
+			System.out.println(i);
+		} // 4 6
+
 	}
+
 }
