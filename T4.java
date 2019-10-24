62afe6c906af49053ed6db6e2b49cb27a725fba3
Alderbane <alderban@outlook.com>
2019-10-24 07:44:59 -0500

    Update T4.java

--- T4.java
+++ T4.java
@@ -40,42 +40,6 @@ public class T4 {
 			return i;
 		} else
 			return -1 * (i + 1);
-
-		// if (arr.length == 0){
-		// exists = false;
-		// return 0;
-		// }
-		// if (arr.length == 1 && arr[0] != n) {
-		// exists = false;
-		// if(arr[0] > n) return 1;
-		// return 0;
-		// // return (arr[0]<n)?1:-1;
-		// }
-		// int half = (arr.length / 2);
-		// if (arr[half] == n){
-		// exists = true;
-		// return half;
-		// }
-
-		// if (arr[half] > n) {
-		// int[] h1 = Arrays.copyOfRange(arr, 0, half);
-		// int iH1 = indexOf(h1, n);
-		// if (exists)
-		// return iH1;
-		// else
-		// return -1 * (1 - iH1);
-		// }
-		// if (arr[half] < n) {
-		// int[] h2 = Arrays.copyOfRange(arr, half , arr.length);
-		// int iH2 = indexOf(h2, n);
-
-		// if (exists)
-		// return half + iH2;
-		// else
-		// return -1 * (half - iH2 + 1);
-		// }
-
-		// return 0;
 	}
 
 	static int indexOf(int[] array, int left, int right, int key) {
@@ -104,7 +68,7 @@ public class T4 {
 
 	static int indexOf(int[] array, int left, int right) {
 		if (left > right)
-			return left;
+			return right;
 		calls++;
 		int middle = (left + right) / 2; // D(N)
 		if (array[middle] == 0)
@@ -145,7 +109,17 @@ public class T4 {
 
 		int[] ded = { -5, -4, -3, -1, 0, 1, 3 };
 		for (int i : maxsubarray(ded)) {
-			System.out.println(i);
+			System.out.printf("%d ", i);
+		} // 4 6
+		System.out.println();
+		int[] ded1 = {0,1,2,3,4,5,6};
+		for (int i : maxsubarray(ded1)) {
+			System.out.printf("%d ", i);
+		} 
+		System.out.println();
+		int[] ded2 = {-1,-2,-3,-4};
+		for (int i : maxsubarray(ded2)) {
+			System.out.printf("%d ", i);
 		} // 4 6
 
 	}
